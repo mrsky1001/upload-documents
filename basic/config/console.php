4981ece81d54c7be27dfa4511f0cdcd2d198d2a1
mrsky1001 <nikita.nk16@yandex.ru>
2020-05-23 23:20:46 +0600

    add heroku

--- basic/config/console.php
+++ basic/config/console.php
@@ -28,6 +28,11 @@ $config = [
         'db' => $db,
     ],
     'params' => $params,
+    'controllerMap' => [
+        'heroku' => [
+            'class' => 'purrweb\heroku\HerokuGeneratorController',
+        ],
+    ],
     /*
     'controllerMap' => [
         'fixture' => [ // Fixture generation command line.
